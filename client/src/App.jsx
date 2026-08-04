8a2035d2cefdf55f03c43a72d3383ffbc99d7d35
johnacarlson <john.algot.carlson@gmail.com>
2026-08-04 15:04:13 -0600

    Add Meta Pixel PageView tracking to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
+import { useEffect } from 'react'
 import Home from './pages/Home'
 
 export default function App() {
+    useEffect(() => {
+          if (window.fbq) {
+                  window.fbq('track', 'PageView');
+          }
+    }, [])
+
   return <Home />
 }
